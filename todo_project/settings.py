d38e648fdf7465c478456c6b2bc0a1b492d132bb
paragoan <sajibulalamsajib1998@gmail.com>
2026-02-22 09:32:57 +0600

    Fixed Allowed Host list.

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = [
     'subclimatic-sherri-nonprovocatively.ngrok-free.dev',
     '.ngrok-free.dev',  # Allow all ngrok-free.dev subdomains
     '.ngrok.io',  # Allow all ngrok.io subdomains (for paid ngrok)
-    'testtttttt-kf9v.onrender.com'
-    ostad-todo.onrender.com,
+    'testtttttt-kf9v.onrender.com',
+    'ostad-todo.onrender.com',
 ]
 
 # CSRF trusted origins for ngrok
